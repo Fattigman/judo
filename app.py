4542edc5bf9e70644c42a422266b0959bf92c048
Fattigman <jacob.karlstrom@gmail.com>
2023-10-22 21:54:04 +0200

    initial commit

---
+++ app.py
@@ -0,0 +1,60 @@
+from flask import Flask, render_template, request, jsonify, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import func
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///judo.db'
+db = SQLAlchemy(app)
+
+class JudoTechnique(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    image_path = db.Column(db.String(100), nullable=False)
+    belt = db.Column(db.String(20), nullable=False)
+    alt_text = db.Column(db.String(50), nullable=True)
+
+    def as_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'image_path': self.image_path,
+            'belt': self.belt
+        }
+
+
+@app.route('/', methods = ['POST', 'GET'])
+def index():
+    return render_template('index.html')
+
+@app.route('/start_quiz', methods=['POST'])
+def start_quiz():
+    belts = request.form.getlist('belts')
+    techniques = JudoTechnique.query.filter(JudoTechnique.belt.in_(belts)).order_by(func.random()).all()
+    print(belts, techniques)
+    if not techniques:
+        # Return some error message or redirect to the index with a message
+        return redirect(url_for('index', message="No techniques found for the selected belts."))
+
+    # Convert the techniques to a list of dictionaries
+    techniques_dict_list = [tech.as_dict() for tech in techniques]
+
+    return render_template('quiz.html', techniques=techniques_dict_list)
+
+
+
+
+@app.route('/check_answer', methods=['POST'])
+def check_answer():
+    user_answer = request.form['answer']
+    technique_id = request.form['technique_id']
+    technique = JudoTechnique.query.get(technique_id)
+    if user_answer.lower() == technique.name.lower():
+        return jsonify(result="Correct!")
+    else:
+        return jsonify(result="Incorrect!")
+
+@app.route('/congratulations')
+def congratulations():
+    return render_template('congratulations.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ init_db.py
@@ -0,0 +1,53 @@
+from app import app, db, JudoTechnique
+
+with app.app_context():
+    db.drop_all()
+    
+    db.create_all()
+
+    techniques = [
+        # Te waza
+        JudoTechnique(name="seoi-nage", belt="yellow", image_path="static/images/seoi_nage.jpeg"),
+        JudoTechnique(name="ippon-seoi-nage", belt="yellow", image_path="static/images/ippon_seoi_nage.jpeg"),
+        JudoTechnique(name="seoi-otoshi", belt="UNKNOWN", image_path="static/images/seoi_otoshi.jpeg"),
+        JudoTechnique(name="tai-otoshi", belt="UNKNOWN", image_path="static/images/tai_otoshi.jpeg"),
+        JudoTechnique(name="kata-guruma", belt="UNKNOWN", image_path="static/images/kata_guruma.jpeg"),
+        JudoTechnique(name="sukui-nage", belt="UNKNOWN", image_path="static/images/sukui_nage.jpeg"),
+        JudoTechnique(name="obi-otoshi", belt="UNKNOWN", image_path="static/images/obi_otoshi.jpeg"),
+        JudoTechnique(name="uki-otoshi", belt="UNKNOWN", image_path="static/images/uki_otoshi.jpeg"),
+        JudoTechnique(name="sumi-otoshi", belt="UNKNOWN", image_path="static/images/sumi_otoshi.jpeg"),
+        JudoTechnique(name="yama-arashi", belt="UNKNOWN", image_path="static/images/yama_arashi.jpeg"),
+        JudoTechnique(name="obi-tori-gaeshi", belt="UNKNOWN", image_path="static/images/obi_tori_gaeshi.jpeg"),
+        JudoTechnique(name="morote-gari", belt="UNKNOWN", image_path="static/images/morote_gari.jpeg"),
+        JudoTechnique(name="kuchiki-taoshi", belt="UNKNOWN", image_path="static/images/kuchiki_taoshi.jpeg"),
+        JudoTechnique(name="kibisu-gaeshi", belt="UNKNOWN", image_path="static/images/kibisu_gaeshi.jpeg"),
+        JudoTechnique(name="uchi-mata-sukashi", belt="UNKNOWN", image_path="static/images/uchi_mata_sukashi.jpeg"),
+        JudoTechnique(name="ko-uchi-gaeshi", belt="UNKNOWN", image_path="static/images/ko_uchi_gaeshi.jpeg"),
+        JudoTechnique(name="de-ashi-harai", belt="UNKNOWN", image_path="static/images/de_ashi_harai.jpeg"),
+        JudoTechnique(name="okuri-ashi-harai", belt="UNKNOWN", image_path="static/images/okuri_ashi_harai.jpeg"),
+        JudoTechnique(name="o-soto-otoshi", belt="yellow", image_path="static/images/o_soto_otoshi.jpeg"),
+        JudoTechnique(name="hiza-guruma", belt="yellow", image_path="static/images/hiza_guruma.jpeg"),
+        JudoTechnique(name="uchi-mata", belt="UNKNOWN", image_path="static/images/uchi_mata.jpeg"),
+        JudoTechnique(name="tsurikomi-goshi", belt="UNKNOWN", image_path="static/images/tsurikomi_goshi.jpeg"),
+        JudoTechnique(name="sasae-tsurikomi-ashi", belt="UNKNOWN", image_path="static/images/sasae_tsurikomi_ashi.jpeg"),
+        JudoTechnique(name="ko-soto-gake", belt="UNKNOWN", image_path="static/images/ko_soto_gake.jpeg"),
+        JudoTechnique(name="o-soto-gari", belt="yellow", image_path="static/images/o_soto_gari.jpeg"),
+        JudoTechnique(name="ashi-guruma", belt="UNKNOWN", image_path="static/images/ashi_guruma.jpeg"),
+        JudoTechnique(name="o-uchi-gari", belt="yellow", image_path="static/images/o_uchi_gari.jpeg"),
+        JudoTechnique(name="harai-tsurikomi-ashi", belt="UNKNOWN", image_path="static/images/harai_tsurikomi_ashi.jpeg"),
+        JudoTechnique(name="ko-soto-gari", belt="yellow", image_path="static/images/ko_soto_gari.jpeg"),
+        JudoTechnique(name="o-guruma", belt="UNKNOWN", image_path="static/images/o_guruma.jpeg"),
+        JudoTechnique(name="ko-uchi-gari", belt="yellow", image_path="static/images/ko_uchi_gari.jpeg"),
+        JudoTechnique(name="o-soto-guruma", belt="UNKNOWN", image_path="static/images/o_soto_guruma.jpeg"),
+        JudoTechnique(name="o-uchi-gaeshi", belt="UNKNOWN", image_path="static/images/o_uchi_gaeshi.jpeg"),
+        JudoTechnique(name="hane-goshi-gaeshi", belt="UNKNOWN", image_path="static/images/hane_goshi_gaeshi.jpeg"),
+        JudoTechnique(name="harai-goshi-gaeshi", belt="UNKNOWN", image_path="static/images/harai_goshi_gaeshi.jpeg"),
+        JudoTechnique(name="tsurikomi-goshi-gaeshi", belt="UNKNOWN", image_path="static/images/tsurikomi_goshi_gaeshi.jpeg"),
+        JudoTechnique(name="uchi-mata-gaeshi", belt="UNKNOWN", image_path="static/images/uchi_mata_gaeshi.jpeg")
+    ]
+
+
+
+    for technique in techniques:
+        db.session.add(technique)
+        db.session.commit()
